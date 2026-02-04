9a820e9c86eb7767ccd8389345fdda11f568977f
djpaulinhoaraujo <djpaulinhoaraujo@gmail.com>
2026-02-04 10:39:02 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return {"ok": True, "msg": "AcheiBarato API rodando"}
+
+@app.get("/buscar")
+def buscar():
+    produto = request.args.get("produto", "").strip()
+
+    if not produto:
+        return jsonify([])
+
+    url = "https://api.mercadolibre.com/sites/MLB/search"
+    params = {
+        "q": produto,
+        "limit": 10
+    }
+
+    resposta = requests.get(url, params=params, timeout=15)
+    dados = resposta.json()
+
+    resultados = []
+
+    for item in dados.get("results", []):
+        resultados.append({
+            "loja": "Mercado Livre",
+            "produto": item.get("title"),
+            "preco": item.get("price"),
+            "link": item.get("permalink"),
+            "imagem": item.get("thumbnail")
+        })
+
+    return jsonify(resultados)
+
+if __name__ == "__main__":
+    app.run()
